a80a5dfbf1303f2100151812606f056aac905d32
Abins Musthafa <musthafaabins@gmail.com>
2019-06-01 12:21:51 +0530

    Initial Commit

---
+++ sottatableCOlle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sottatableCOlle
+//
+//  Created by Abins Musthafa on 01/06/19.
+//  Copyright © 2019 Abins Musthafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
